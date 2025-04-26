81ec77ecc81172b2679ce74873c504541a6491e8
Rashmitha Kalhara <141613341+rashmithaRKL@users.noreply.github.com>
2025-04-26 15:11:06 +0530

    deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: "/pixel-store/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
